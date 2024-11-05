478bac90fdb2e7ab9128a14aa7fe468b1e0402a9
Diego <88857178+DiegoBr7@users.noreply.github.com>
2024-11-05 14:49:09 -0300

    Add files via upload

---
+++ fiap-gateway/src/test/java/br/com/fiap/fiap_gateway/FiapGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiap_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
